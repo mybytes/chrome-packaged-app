710bd78794f99174ebd535a9198c1da26fe7431c
rob <rob@robs-xps-laptop.(none)>
2012-12-30 13:53:24 -0800

    add fade animation directive

--- app/js/directives.js
+++ app/js/directives.js
@@ -2,10 +2,24 @@
 
 /* Directives */
 
-
 angular.module('myApp.directives', []).
   directive('appVersion', ['version', function(version) {
     return function(scope, elm, attrs) {
+      console.log('appVersion...');
       elm.text(version);
     };
-  }]);
+  }]); 
+
+angular.module('myApp.directives', []).
+  directive('fadeIn', function() {
+    return {
+      compile: function(elm) {
+        console.log('compiling');
+        $(elm).css('opacity', 0.1);
+        return function(scope, elm, attrs) {
+          console.log('animating');
+          $(elm).animate({ opacity : 1.0 }, 1000 );
+        };
+      }
+    };
+});

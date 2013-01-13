7d72fa58a547e3604c911903da892e93ac1a5eb6
rob <rob@rob-xps.(none)>
2013-01-13 13:17:39 -0800

    added carousel, improved tooltips

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,15 +3,15 @@
 function createDataGrid($scope, $http, dataUrl) {
 
     $scope.filterOptions = {
-        filterText: "",
-        useExternalFilter: true
+        filterText:"",
+        useExternalFilter:true
     };
 
     $scope.pagingOptions = {
-        pageSizes: [10, 25, 100],
-        pageSize: 10,
-        totalServerItems: 15,
-        currentPage: 1
+        pageSizes:[10, 25, 100],
+        pageSize:10,
+        totalServerItems:15,
+        currentPage:1
     };
 
     $scope.getPagedDataAsync = function (pageSize, page, searchText) {
@@ -79,44 +79,47 @@ function createDataGrid($scope, $http, dataUrl) {
     $scope.url = "";
 
     $scope.gridOptions = {
-        canSelectRows: true,
-        multiSelect: false,
-        jqueryUITheme: true,
-        displaySelectionCheckbox: false,
-        data: 'servicesData',
-        selectedItems: $scope.mySelections,
-        enablePaging: true,
-        pagingOptions: $scope.pagingOptions,
-        filterOptions: $scope.filterOptions
+        canSelectRows:true,
+        multiSelect:false,
+        jqueryUITheme:true,
+        displaySelectionCheckbox:false,
+        data:'servicesData',
+        selectedItems:$scope.mySelections,
+        enablePaging:true,
+        pagingOptions:$scope.pagingOptions,
+        filterOptions:$scope.filterOptions
     };
 
 }
 
-myApp.controller('HomeCtrl', ['$scope', function($scope) {}]);
+myApp.controller('HomeCtrl', ['$scope', function ($scope) {
+}]);
 
-myApp.controller('AboutCtrl', ['$scope', function($scope) {}]);
+myApp.controller('AboutCtrl', ['$scope', function ($scope) {
+}]);
 
-myApp.controller('ContactCtrl', ['$scope', function($scope) {}]);
+myApp.controller('ContactCtrl', ['$scope', function ($scope) {
+}]);
 
-myApp.controller('ConfigureCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
+myApp.controller('ConfigureCtrl', ['$scope', '$routeParams', function ($scope, $routeParams) {
     console.log("configure!");
     $scope.serviceName = $routeParams.serviceName;
     console.log($scope.serviceName);
 
-    $scope.master= {};
+    $scope.master = {};
 
-    $scope.update = function(user) {
-        $scope.master= angular.copy(user);
+    $scope.update = function (user) {
+        $scope.master = angular.copy(user);
     };
 
-    $scope.reset = function() {
+    $scope.reset = function () {
         $scope.user = angular.copy($scope.master);
     };
 
     $scope.reset();
 
 }]);
-var playground = myApp.controller('PlaygroundCtrl', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
+var playground = myApp.controller('PlaygroundCtrl', ['$scope', '$routeParams', '$http', function ($scope, $routeParams, $http) {
     // add service name to the scope...
     $scope.widgetName = $routeParams.widgetName;
     $scope.widgetUrl = "partials/playground/" + $routeParams.widgetName + ".html"
@@ -126,15 +129,17 @@ var playground = myApp.controller('PlaygroundCtrl', ['$scope', '$routeParams', '
 
 
     // tree support
-    $scope.deleteNode = function(data) {
+    $scope.deleteNode = function (data) {
         data.nodes = [];
     };
-    $scope.addNode = function(data) {
+    $scope.addNode = function (data) {
         var post = data.nodes.length + 1;
         var newName = data.name + '-' + post;
-        data.nodes.push({name: newName,nodes: []});
+        data.nodes.push({name:newName, nodes:[]});
     };
-    $scope.tree = [{name: "Node", nodes: []}];
+    $scope.tree = [
+        {name:"Node", nodes:[]}
+    ];
 
 
     // data grid
@@ -170,31 +175,42 @@ var playground = myApp.controller('PlaygroundCtrl', ['$scope', '$routeParams', '
 
     // calendar
 
-       var date = new Date();
-        var d = date.getDate();
-        var m = date.getMonth();
-        var y = date.getFullYear();
+    var date = new Date();
+    var d = date.getDate();
+    var m = date.getMonth();
+    var y = date.getFullYear();
 
-        $scope.events = [
-            {title: 'All Day Event',start: new Date(y, m, 1)},
-            {title: 'Long Event',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2)},
-            {id: 999,title: 'Repeating Event',start: new Date(y, m, d - 3, 16, 0),allDay: false},
-            {id: 999,title: 'Repeating Event',start: new Date(y, m, d + 4, 16, 0),allDay: false},
-            {title: 'Birthday Party',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),allDay: false},
-            {title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
-        ]
+    $scope.events = [
+        {title:'All Day Event', start:new Date(y, m, 1)},
+        {title:'Long Event', start:new Date(y, m, d - 5), end:new Date(y, m, d - 2)},
+        {id:999, title:'Repeating Event', start:new Date(y, m, d - 3, 16, 0), allDay:false},
+        {id:999, title:'Repeating Event', start:new Date(y, m, d + 4, 16, 0), allDay:false},
+        {title:'Birthday Party', start:new Date(y, m, d + 1, 19, 0), end:new Date(y, m, d + 1, 22, 30), allDay:false},
+        {title:'Click for Google', start:new Date(y, m, 28), end:new Date(y, m, 29), url:'http://google.com/'}
+    ]
 
-        $scope.addChild = function() {
-            $scope.events.push({
-                title: 'Open Sesame',
-                start: new Date(y, m, 28),
-                end: new Date(y, m, 29)
-            });
-        }
+    $scope.addChild = function () {
+        $scope.events.push({
+            title:'Open Sesame',
+            start:new Date(y, m, 28),
+            end:new Date(y, m, 29)
+        });
+    }
 
-        $scope.remove = function(index) {
-            $scope.events.splice(index,1);
-        }
+    $scope.remove = function (index) {
+        $scope.events.splice(index, 1);
+    }
 
+
+    // carousel
+    $scope.slides = [
+        {image:'http://placekitten.com/325/200', text:'Kitten.'},
+        {image:'http://placekitten.com/275/200', text:'Kitty!'},
+        {image:'http://placekitten.com/375/200', text:'Cat.'},
+        {image:'http://placekitten.com/250/200', text:'Feline!'}
+    ];
+    $scope.selectSlide = function (i) {
+        $scope.selected = i;
+    };
 
 }]);
